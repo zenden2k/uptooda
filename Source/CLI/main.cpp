9c5769fdfef29f34dbed735ece5b75351041c7df
Sergey Svistunov <zenden2k@gmail.com>
2025-10-11 08:41:16 +0300

    CLI: fix unicode output

--- Source/CLI/main.cpp
+++ Source/CLI/main.cpp
@@ -193,7 +193,9 @@ void OnUploadSessionFinished(UploadSession* session) {
     //ConsoleUtils::instance()->SetCursorPos(0, taskCount + 2);
 
     std::cerr<<std::endl<<"Result:"<<std::endl;
-    std::cout<< generator->generate(uploadedList);
+
+    ConsoleUtils::instance()->printUnicode(stdout, generator->generate(uploadedList));
+
     std::cerr<<std::endl;
 
     {
