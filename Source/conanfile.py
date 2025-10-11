129710128782558c7c1320aaed946fe3c593fdd8
Sergey Svistunov <zenden2k@gmail.com>
2025-10-11 08:33:20 +0300

    Android

--- Source/conanfile.py
+++ Source/conanfile.py
@@ -65,7 +65,7 @@ class ImageUploaderRecipe(ConanFile):
         self.requires("minizip/1.3.1")
         self.requires("jsoncpp/1.9.6")
         self.requires("sqlite3/3.49.1")
-        self.requires("base64/0.4.0")
+        self.requires("base64/0.5.2")
         self.requires("glog/0.6.0@zenden2k/stable")
         self.requires("libwebp/1.4.0", force=True)
         self.requires("gtest/1.10.0")
